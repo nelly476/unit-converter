71ca59afd27a5d47b7c0f826e703a565d2edd66f
nelly476 <107814052+nelly476@users.noreply.github.com>
2022-10-07 15:03:03 +0300

    add localStorage

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const inputEl = document.querySelector("#input-el");
 const lengthEl = document.querySelector("#length-el");
 const volumeEl = document.querySelector("#volume-el");
 const massEl = document.querySelector("#mass-el");
+const metricsFromLocalStorage = JSON.parse(
+  localStorage.getItem("addedMetrics")
+);
 
-convertBtn.addEventListener("click", function () {
+if (metricsFromLocalStorage) {
+  lengthEl.innerHTML = metricsFromLocalStorage[0];
+  volumeEl.textContent = metricsFromLocalStorage[1];
+  massEl.textContent = metricsFromLocalStorage[2];
+}
+
+let addedMetrics = [];
+
+convertBtn.addEventListener("click", function (e) {
   e.preventDefault();
   lengthEl.textContent = "";
   volumeEl.textContent = "";
@@ -22,4 +33,9 @@ ${inputEl.value} gallons = ${(inputEl.value / 0.264).toFixed(2)} litres
 ${inputEl.value} kilos = ${(inputEl.value * 2.204).toFixed(2)} pounds |
 ${inputEl.value} pounds = ${(inputEl.value / 2.204).toFixed(2)} kilos
 `;
+
+  addedMetrics.push(lengthEl.textContent);
+  addedMetrics.push(volumeEl.textContent);
+  addedMetrics.push(massEl.textContent);
+  localStorage.setItem("addedMetrics", JSON.stringify(addedMetrics));
 });
